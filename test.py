d603db0d7b1b0cd030e28ea67f37cb18f6afe08d
André Gaul <gaul@web-yard.de>
2012-12-21 18:47:00 +0100

    use hmax()

--- test.py
+++ test.py
@@ -5,33 +5,40 @@ solution: u(x,t) = sin(pi*t)*exp(-norm(x)^2)
 '''
 
 from dolfin import *
+import numpy as np
+from matplotlib import pyplot as pp
+
 normstr = '(x[0]*x[0]+ x[1]*x[1] + x[2]*x[2])'
 u_ex = Expression('sin(pi*t)*exp(-' + normstr + ')', t=0)
 f = Expression('(pi*cos(pi*t) - sin(pi*t)*(4*' + normstr + ' - 6))*exp(-' + normstr + ')', t=0)
 
-def solve_heat(n):
-    mesh = UnitCube(n, n, n)
+def solve_heat(p):
+    mesh = UnitCube(2**p, 2**p, 2**p)
+    hmax = mesh.hmax()
     V = FunctionSpace(mesh, 'CG', 1)
     u = TrialFunction(V)
     v = TestFunction(V)
     bc = DirichletBC(V, u_ex, 'on_boundary')
 
-    u_old = Function(V)
-    u_old.interpolate(u_ex)
-    u_new = Function(V)
-
     t = 0
-    dt = 1./n
+    u_ex.t = 0
+    f.t = 0
+    dt = 1./(2**p)
     tend = 2
 
     Avar = u*v*dx + dt*inner(grad(u),grad(v))*dx
 
     L2errors = []
 
+    u_old = Function(V)
+    u_old.interpolate(u_ex)
+    u_new = Function(V)
+
     while t<tend:
         t += dt
         u_ex.t = t
         f.t = t
+        print(t)
 
         bvar = (u_old + dt*f)*v*dx
         solve( Avar == bvar, u_new, bc)
@@ -39,14 +46,21 @@ def solve_heat(n):
         u_old = u_new.copy()
         L2errors += [errornorm(u_ex,u_new)]
 
-    return max(L2errors)
+    return hmax, max(L2errors)
 
+hmaxs = []
 errors = []
-P = range(1,5)
+P = range(1,6)
 for p in P:
-    errors += [solve_heat(2**p)]
+    hmax, err = solve_heat(p)
+    hmaxs += [hmax]
+    errors += [err]
 
-from matplotlib import pyplot as pp
-pp.loglog( [1./(2**p) for p in P], errors , '-o')
-pp.loglog( [1./(2**P[0]), 1./(2**P[-1])], [errors[0], errors[0]*(1./(2**(P[-1]-P[0])))])
+hmaxs = np.array(hmaxs)
+errors = np.array(errors)
+eoc = np.log(errors[1:]/errors[:-1])/np.log(hmaxs[1:]/hmaxs[:-1])
+print(eoc)
+
+pp.loglog( hmaxs, errors , '-o')
+pp.loglog( [hmaxs[0],hmaxs[-1]], [errors[0], errors[0]*(hmaxs[-1]/hmaxs[0])])
 pp.show()
