b08b3e46384ba38d188858249d0293d407ef41da
André Gaul <gaul@web-yard.de>
2012-12-24 16:44:39 +0100

    EOC with Dirichlet+Robin boundary conditions

--- test.py
+++ test.py
@@ -11,16 +11,35 @@ from matplotlib import pyplot as pp
 normstr = '(x[0]*x[0]+ x[1]*x[1] + x[2]*x[2])'
 u_ex = Expression('sin(pi*t)*exp(-' + normstr + ')', t=0)
 f = Expression('(pi*cos(pi*t) - sin(pi*t)*(4*' + normstr + ' - 6))*exp(-' + normstr + ')', t=0)
+u_ex_grad = Expression( (
+        '-2*sin(pi*t)*x[0]*exp(-' + normstr + ')',
+        '-2*sin(pi*t)*x[1]*exp(-' + normstr + ')',
+        '-2*sin(pi*t)*x[2]*exp(-' + normstr + ')'
+        ), t=0)
+# robin boundary -dot(grad(u),n) = alpha*(u - q)
+alpha = Constant(1.0)
 
-#mesh = UnitCube(2,2,2)
-mesh = Mesh('msh_pan.xml')
+class RobinBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0]<0.5
 
-def solve_heat(mesh, p, scale_dt=1.0):
+mesh = UnitCube(2,2,2)
+#mesh = Mesh('msh_pan.xml')
+
+def solve_heat(mesh, p, boundaries=None, scale_dt=1.0):
     hmax = mesh.hmax()
     V = FunctionSpace(mesh, 'CG', 1)
     u = TrialFunction(V)
     v = TestFunction(V)
-    bc = DirichletBC(V, u_ex, 'on_boundary')
+    if boundaries is None:
+        boundaries = MeshFunction("uint", mesh, mesh.topology().dim()-1)
+        boundaries.set_all(1)
+    # tag 0: Dirichlet
+    # tag 1: Robin
+    ds = Measure("ds")[boundaries]
+    bc = DirichletBC(V, u_ex, boundaries, 0)
+
+    n = FacetNormal(mesh)
 
     t = 0.
     u_ex.t = t
@@ -30,7 +49,9 @@ def solve_heat(mesh, p, scale_dt=1.0):
 
     print("Solve with hmax=%e (%d unknowns) and dt=%e." % (hmax, V.dim(), dt))
 
-    Avar = u*v*dx + dt*inner(grad(u),grad(v))*dx
+    Avar = u*v*dx + dt*inner(grad(u),grad(v))*dx + dt*alpha*u*v*ds(1)
+    # Robin boundary function
+    q = u_ex + (1/alpha)*dot(u_ex_grad, n)
 
     L2errors = []
 
@@ -42,10 +63,11 @@ def solve_heat(mesh, p, scale_dt=1.0):
         t += dt
         u_ex.t = t
         f.t = t
+        u_ex_grad.t = t
 
-        bvar = (u_old + dt*f)*v*dx
+        bvar = (u_old + dt*f)*v*dx + dt*alpha*q*v*ds(1)
 
-        A, b = assemble_system(Avar, bvar, bc)
+        A, b = assemble_system(Avar, bvar, bc, exterior_facet_domains=boundaries)
 
         solve(A, u_new.vector(), b, "cg", "amg")
 
@@ -58,7 +80,13 @@ hmaxs = []
 errors = []
 P = range(0,3)
 for p in P:
-    hmax, err = solve_heat(mesh, p, 0.2)
+    # mark Robin boundary condition
+    boundaries = MeshFunction("uint", mesh, mesh.topology().dim()-1)
+    boundaries.set_all(0)
+    robin = RobinBoundary()
+    robin.mark(boundaries, 1)
+
+    hmax, err = solve_heat(mesh, p, boundaries=boundaries, scale_dt=0.2)
     hmaxs += [hmax]
     errors += [err]
     mesh = refine(mesh)
@@ -66,6 +94,7 @@ for p in P:
 hmaxs = np.array(hmaxs)
 errors = np.array(errors)
 eoc = np.log(errors[1:]/errors[:-1])/np.log(hmaxs[1:]/hmaxs[:-1])
+print(errors)
 print(eoc)
 
 pp.loglog( hmaxs, errors , '-o')
